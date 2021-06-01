c7ebdd6ed67a42ff47276b3be81192cb7e96e28e
yash handa <yashhanda500@gmail.com>
2021-06-01 22:00:55 +0530

    update

--- webbasics/validation.js
+++ webbasics/validation.js
@@ -9,6 +9,25 @@ let myfun=function(a){
 }
 
 let submitted=function(){
-    let val=document.querySelector("#input1").value;
+    let val=document.querySelector(".input1").value;
     console.log(val);
 }
+
+// document.querySelector("#myform").addEventListener("submit",(event)=>{
+//     event.preventDefault();
+//     console.log(event.target.input1.value);
+//     console.log(event.target.input2.value);
+//     event.target.input1.value="";
+// });
+
+document.querySelector(".form2").addEventListener("submit",(event)=>{
+    let p1=event.target.pass.value;
+    let p2=event.target.repass.value;
+    event.preventDefault();
+    if(p1==p2){
+        console.log("successful registration");
+    }
+    else{
+        console.log("password dosen't match");
+    }
+})
